ec44d684ecc7882420237bc7d49b912c339feb46
Taljjaa <jenken131@gmail.com>
2019-12-30 21:02:31 -0800

    added organization info to card and started org show page

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import { Card, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
-const EventCard = ({ event: { title, location, description } }) => {
+const EventCard = ({
+   event: {
+      title,
+      location,
+      description,
+      organization: { display_name, logo, id },
+   },
+}) => {
    return (
       <Card>
          <Card.Content>
+            <Image src={logo} wrapped ui={false} />
             <Card.Header>{title}</Card.Header>
-            <Card.Meta>{location}</Card.Meta>
+            <Link to={`/organizations/${id}`}>
+               <Card.Meta>{`By ${display_name}`}</Card.Meta>
+            </Link>
             <Card.Description>{description}</Card.Description>
          </Card.Content>
+         <Card.Content extra>{location}</Card.Content>
       </Card>
    );
 };
---
+++ src/components/OrganizationContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class OrganizationContainer extends React.Component {
+   render() {
+      return <div>Hi</div>;
+   }
+}
+
+export default OrganizationContainer;
